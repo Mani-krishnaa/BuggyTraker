ec8e329eec919909a6fce0215d9995a46c87ccc8
ManiKrishna <mani@hackerearth.com>
2025-09-28 18:41:40 +0530

    feat: implement ApiResponse class for checking API response reachability

---
+++ utils/apiResponse.ts
@@ -0,0 +1,22 @@
+
+import { Page } from '@playwright/test';
+import { BasePage } from '../pages/basePage';
+
+
+export class ApiRespons extends BasePage {
+
+    constructor(page: Page) {
+        super(page)
+    }
+    async checkingApiResponse(url: string): Promise<string> {
+
+        try {
+            const response = await this.page.request.get(url);
+            return response.ok() ? "reachable" : "unreachable"
+        }
+        catch (e) {
+            return "unreachable"
+        }
+
+    }
+}
