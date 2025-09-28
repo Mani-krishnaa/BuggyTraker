67918a7469f89eacd3eb6e4af823f6302ae36d34
ManiKrishna <mani@hackerearth.com>
2025-09-28 17:13:54 +0530

    feat: implement BasePage class for common page object functionality

---
+++ pages/basePage.ts
@@ -0,0 +1,16 @@
+import { Page } from "@playwright/test";
+/**
+ * Base page class that all page objects inherit from
+ * Contains common methods and properties
+ */
+export class BasePage {
+    protected page: Page;
+
+    /**
+     * Constructor for the base page
+     * @param page - Playwright page object
+     */
+    constructor(page: Page) {
+        this.page = page;
+    }
+}
