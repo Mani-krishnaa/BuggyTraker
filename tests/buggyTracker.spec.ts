e4e6a1216b1fe5736e19728f4805fb481fde84df
ManiKrishna <mani@hackerearth.com>
2025-09-28 18:41:54 +0530

    feat: enhance buggyTracker test to include API response validation

--- tests/buggyTracker.spec.ts
+++ tests/buggyTracker.spec.ts
@@ -1,10 +1,16 @@
 
 
-import { test, } from '/home/hackerearth461/BuggyTraker/tests/Fixtures.ts'
+import { expect, test, } from '/home/hackerearth461/BuggyTraker/tests/Fixtures.ts'
 
-test.describe("Buggy Tracker Challange",() => {
-  test('has title', async ({ analytics }) => {
+test.describe("Buggy Tracker Challange", () => {
+  test('has title', async ({ analytics, apiresponse }) => {
     const link = await analytics.returnAnalyticsLink()
+    console.log(link);
+
+    const response = await apiresponse.checkingApiResponse(link!)
+    const mani = response == "reachable" ? console.log("it is valid link") : console.log("it is invalid link");
+    ;
+
 
   });
 })
