1cb09b6917ace64d38c898b31aa1a80fced21560
ManiKrishna <mani@hackerearth.com>
2025-10-09 12:59:19 +0530

    feat: improve Buggy Tracker Challenge test by adding retry logic for saving changes in EditCard

--- tests/buggyTracker.spec.ts
+++ tests/buggyTracker.spec.ts
@@ -1,9 +1,8 @@
 
-
 import { expect, test, } from '/home/hackerearth461/BuggyTraker/tests/Fixtures.ts'
 
 test.describe("Buggy Tracker Challenge", () => {
-  test('Checking the response of the link which is inside the Iframe', async ({ analytics, apiresponse, dragdrop, editcard }) => {
+  test('Checking the response of the link which is inside the Iframe', async ({ analytics, apiresponse, dragdrop, editcard, page }) => {
 
     const link = await analytics.returnAnalyticsLink()
     const response = await apiresponse.checkingApiResponse(link!)
@@ -15,8 +14,17 @@ test.describe("Buggy Tracker Challenge", () => {
     await editcard.changeWorkingToDone()
     await editcard.changeAssigneTo()
     await editcard.changeDeadline()
-    await editcard.saveChangesButtonInForm()
-    expect(await editcard.savingTicket.waitFor({ state: "visible" }))
+    while (true) {
+      await editcard.saveChangesButtonInForm()
+      try {
+        expect(await editcard.savingTicket.waitFor({ state: "visible" }))
+        expect(await editcard.movedTicket.waitFor({ state: "visible" }))
+        console.log("Moved ticket is now visible!");
+        break;
+      } catch (error) {
+        await new Promise(resolve => setTimeout(resolve, 100))
+      }
+    }
 
   });
 
